e8614e39cb35e6590f343d8fb62e9bb5b1cee24c
joseN <navarretejose073@gmail.com>
2022-09-26 15:04:50 -0300

    correccion de imports

--- jona_app1/views.py
+++ jona_app1/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.https import HttpsResponse
+from django.http import HttpResponse
 # Create your views here.
 def displayTabla(request):
-    return HttpsResponse("<h1>Bienbenido</h1><p>soy un parrafo</p><table> <th>Hola</th><tr><td>wenas</td></tr> </table>")
+    return HttpResponse("<h1>Bienbenido</h1><p>soy un parrafo</p><table> <th>Hola</th><tr><td>wenas</td></tr> </table>")
 
 def displayFormulario(request):
-    return HttpsResponse("<form><label>formulario</label><input type='text'>Ingrese nombre: </input></form>")
+    return HttpResponse("<form><label>formulario</label><input type='text'>Ingrese nombre: </input></form>")
--- jona_app2/views.py
+++ jona_app2/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpsResponse
+from django.http import HttpResponse
 # Create your views here.
 def displayList(request):
-    return(HttpsResponse("<header>Holaaaa!</header><ul><li>soy una lista</li></ul><div><a href='https://www.youtube.com/watch?v=BuOADNq99Ms'></a></div>"))
+    return(HttpResponse("<header>Holaaaa!</header><ul><li>soy una lista</li></ul><div><a href='https://www.youtube.com/watch?v=BuOADNq99Ms'></a></div>"))
 
 def displayBotones(request):
-    return(HttpsResponse("<form><label>formulario</label><input type='text'>Ingrese nombre: </input><input type='text'>Ingrese correo: </input><button>Enviar</button></form>"))
+    return(HttpResponse("<form><label>formulario</label><input type='text'>Ingrese nombre: </input><input type='text'>Ingrese correo: </input><button>Enviar</button></form>"))
